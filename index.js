6201b85bb1b322db7fad9de23e33e5c9da0ff117
Vented5 <venteddiagram@gmail.com>
2025-01-28 02:33:17 -0600

    setup(db):Server basic config, prisma package added ando connected to mongo cluster, siple collection (scores) endpoints added

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.post('/scores', async (req, res) => {
+    const { tag, score } = req.body
+    const newScore = await prisma.highScore.create({
+        data: {
+            tag,
+            score
+        }
+    })
+    res.json(newScore)
+})
+
+app.get('/scores', async (req, res) => {
+    const scores = await prisma.highScore.findMany();
+})
+
+app.listen(port, () => {
+    console.log(`Larga noche de consuelo escuchando en http://localhost:${port}`)
+})
