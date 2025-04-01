e61e346026d00d46f71edb4e5553a84c394d49c4
Vented5 <venteddiagram@gmail.com>
2025-04-01 04:00:12 -0500

    fix(cors):cors added.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
@@ -6,6 +7,7 @@ const app = express()
 const port = 3000
 
 app.use(express.json())
+app.use(cors())
 
 app.post('/scores', async (req, res) => {
     const { tag, score, character, weapon } = req.body
