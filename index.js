b26aac27f021b0ebd3520aa4b539085cbc931e6f
Vented5 <venteddiagram@gmail.com>
2025-04-03 05:16:57 -0500

    cors error fix

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = 3000
 app.use(express.json())
 ///app.use(cors())
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'http://localhost:5173/'
 }));
 
 app.post('/scores', async (req, res) => {
